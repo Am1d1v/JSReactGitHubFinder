e4c976642b43a99cddc1b723729b5991d734687e
Am1d1v <dimadiman1adima@gmail.com>
2024-01-24 23:37:09 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import { BrowserRouter as  Router, Route } from 'react-router-dom';
+import { BrowserRouter as  Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
     <Router>
         <NavBar title="GitHub Finder"/>
+
+        <main className='container'>
+
+          {/* Home Route */}
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+
+          {/* About Route */}
+          <Routes>
+            <Route path='/about' element={<About />} />
+          </Routes>
+
+          {/* Page NotFound Route */}
+          <Routes>
+            <Route path='/asd' element={<NotFound />} />
+          </Routes>
+
+
+        </main>
     </Router>
   );
 }
